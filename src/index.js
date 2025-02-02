dc2765c5380b748994f7656b9e83ae93c14dbc34
jmg24a <jmg24a@gmail.com>
2025-02-02 17:01:17 -0400

    fix index

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const appRouter = require('./router');
 //constants
 const app = express();
 const port = process.env.PORT || 3000;
-const whiteList = ["http://localhost:3000"];
+const whiteList = [`http://localhost:${port}`];
 const optionsCors = {
   origin: (origin, callback) => {
     console.log("HELLO")
